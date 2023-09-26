14a4d0d16fef9409157dd6286839f8a2f5df0532
DinDotDout <joandot1998@gmail.com>
2023-09-26 14:02:14 +0200

    some fixes for auto installer

--- hyprland-install.sh
+++ hyprland-install.sh
@@ -1,40 +1,42 @@
-pacmanPckgs=(
-    "hyprland"
-    "wl-clipboard"
-    "xdg-desktop-portal-hyprland" 
-    "waybar" 
-    "grim" 
-    "slurp"
-    "hyprpicker"
-    "wofi"
-);
-
-paruPckgs=(
-    "swww" 
-    "swaylock-effects" 
-    "wlogout"
-    "pyprland"
-);
-
-sudo pacman --noconfirm --needed -S pacmanPckgs
-sudo paru --noconfirm --needed -S paruPckgs
+#!/bin/bash
+sudo pacman --noconfirm --needed -S hyprland wl-clipboard xdg-desktop-portal-hyprland waybar grim slurp wofi
+paru --noconfirm --needed -S swww swaylock-effects wlogout pyprland hyprpicker
 
 stow -R hyprland-conf
+
+confirm() {
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    # call with a prompt string or use a default
+    # read -r -p "${1:(yY/nN)} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
 
 # add hyprand needed modifications
 # open files to edit manually with instructions
 echo "In /etc/default/grub add nvidia_drm.modeset=1 to GRUB_CMDLINE_LINUX_DEFAULT="
+confirm "Do you want the command to be coppied to your clipboard?" && wl-copy "GRUB_CMDLINE_LINUX_DEFAULT=nvidia_drm.modeset=1"
+# echo -p "Do you want the command to be coppied to your clipboard? (yY/Nn)" yn
 read -p "Open file on press" c
 sudo nvim /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "In /etc/mkinitcpio.conf add nvidia nvidia_modeset nvidia_uvm nvidia_drm to your MODULES"
+confirm "Do you want the command to be coppied to your clipboard?" && wl-copy "nvidia nvidia_modeset nvidia_uvm nvidia_drm"
 read -p "Open file on press" c
 sudo nvim /etc/default/grub
 mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
 
 echo "add a new line to /etc/modprobe.d/nvidia.conf (make it if it does not exist) and add the line options nvidia-drm modeset=1"
+confirm "Do you want the command to be coppied to your clipboard?" && wl-copy "options nvidia-drm modeset=1"
 read -p "Open file on press" c
 sudo nvim /etc/modprobe.d/nvidia.conf
 echo "Done!"
 echo "You can reboot to see the changes"
+
--- i3-install.sh
+++ i3-install.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 sudo pacman --needed --noconfirm -S polybar i3-gaps picom flameshot arandr xorg-xrandr xclip
-sudo paru --needed --noconfirm -S i3lock-color
+paru --needed --noconfirm -S i3lock-color
 stow -R i3-conf
 echo "Addin permissions to lock screen script"
 chmod +x $HOME/.local/bin/lock
