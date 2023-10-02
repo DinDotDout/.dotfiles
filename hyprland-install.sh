95256b4f70ec7dd498fba5eb6bf90a365e10a6d3
DinDotDout <joandot1998@gmail.com>
2023-10-02 18:49:32 +0200

    nvidia hypr install fix

--- hyprland-install.sh
+++ hyprland-install.sh
@@ -20,23 +20,16 @@ confirm() {
 
 # add hyprand needed modifications
 # open files to edit manually with instructions
-echo "In /etc/default/grub add nvidia_drm.modeset=1 to GRUB_CMDLINE_LINUX_DEFAULT="
-confirm "Do you want the command to be coppied to your clipboard?" && wl-copy "GRUB_CMDLINE_LINUX_DEFAULT=nvidia_drm.modeset=1"
-# echo -p "Do you want the command to be coppied to your clipboard? (yY/Nn)" yn
-read -p "Open file on press" c
-sudo nvim /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+echo "Add nvidia_drm.modeset=1 to GRUB_CMDLINE_LINUX_DEFAULT= in /etc/default/grub"
+confirm "Do you want the command to be coppied to your clipboard?" && sudo vim -c "let @\"='GRUB_CMDLINE_LINUX_DEFAULT=nvidia_drm.modeset=1'" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "In /etc/mkinitcpio.conf add nvidia nvidia_modeset nvidia_uvm nvidia_drm to your MODULES"
-confirm "Do you want the command to be coppied to your clipboard?" && wl-copy "nvidia nvidia_modeset nvidia_uvm nvidia_drm"
-read -p "Open file on press" c
-sudo nvim /etc/default/grub
-mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+confirm "Do you want the command to be coppied to your clipboard?" && sudo vim -c "let @\"='nvidia nvidia_modeset nvidia_uvm nvidia_drm'" /etc/mkinitcpio.conf
+sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
 
-echo "add a new line to /etc/modprobe.d/nvidia.conf (make it if it does not exist) and add the line options nvidia-drm modeset=1"
-confirm "Do you want the command to be coppied to your clipboard?" && wl-copy "options nvidia-drm modeset=1"
-read -p "Open file on press" c
-sudo nvim /etc/modprobe.d/nvidia.conf
+echo "Add a new line to /etc/modprobe.d/nvidia.conf (make it if it does not exist) and add the line options nvidia-drm modeset=1"
+confirm "Do you want the command to be coppied to your clipboard?" && sudo vim -c "let @\"='options nvidia-drm modeset=1'" /etc/modprobe.d/nvidia.conf
 echo "Done!"
 echo "You can reboot to see the changes"
 
