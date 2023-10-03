551235db23ffffe26af31f5ded9fc9c66276e150
DinDotDout <joandot1998@gmail.com>
2023-10-03 22:18:32 +0200

    Copy promts improved logic

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -6,9 +6,9 @@ stow -R hyprland-conf
 
 confirm() {
     # call with a prompt string or use a default
-    read -r -p "${1:(yY/nN)} " response
+    read -r -p "${1:-(yY/nN)} " response
     case "$response" in
-        [yY][eE][sS]|[yY]) 
+        [yY][eE][sS]|[yY]|"") 
             true
             ;;
         *)
@@ -22,18 +22,20 @@ if [ -z "$isnvidia" ]; then
   exit
 fi
 
+msg="Do you want the command to be coppied to your vim clipboard?"
+
 # add hyprand needed modifications
 # open files to edit manually with instructions
 echo "Add nvidia_drm.modeset=1 to GRUB_CMDLINE_LINUX_DEFAULT= in /etc/default/grub"
-confirm "Do you want the command to be coppied to your clipboard?" && sudo vim -c "let @\"='GRUB_CMDLINE_LINUX_DEFAULT=nvidia_drm.modeset=1'" /etc/default/grub
+(confirm "$msg" && sudo vim -c "let @\"='GRUB_CMDLINE_LINUX_DEFAULT=nvidia_drm.modeset=2'" /etc/default/grub) || sudo vim /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "In /etc/mkinitcpio.conf add nvidia nvidia_modeset nvidia_uvm nvidia_drm to your MODULES"
-confirm "Do you want the command to be coppied to your clipboard?" && sudo vim -c "let @\"='nvidia nvidia_modeset nvidia_uvm nvidia_drm'" /etc/mkinitcpio.conf
+(confirm "$msg" && sudo vim -c "let @\"='nvidia nvidia_modeset nvidia_uvm nvidia_drm'" /etc/mkinitcpio.conf) || vim /etc/mkinitcpio.conf
 sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
 
 echo "Add a new line to /etc/modprobe.d/nvidia.conf (make it if it does not exist) and add the line options nvidia-drm modeset=1"
-confirm "Do you want the command to be coppied to your clipboard?" && sudo vim -c "let @\"='options nvidia-drm modeset=1'" /etc/modprobe.d/nvidia.conf
+(confirm "$msg" && sudo vim -c "let @\"='options nvidia-drm modeset=1'" /etc/modprobe.d/nvidia.conf) || vim /etc/modprobe.d/nvidia.conf
 echo "Done!"
 echo "You can reboot to see the changes"
 
