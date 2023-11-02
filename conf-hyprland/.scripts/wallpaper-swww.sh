cff5698f6cf5d7f56be2eacaa5c429d4b9cfc962
DinDotDout <joandot1998@gmail.com>
2023-11-02 14:13:06 +0100

    find wallpapers can now follow symlinks

--- conf-hyprland/.scripts/wallpaper-swww.sh
+++ conf-hyprland/.scripts/wallpaper-swww.sh
@@ -1,54 +1,60 @@
 #!/bin/bash
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --wallpaper) selected="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
+	case $1 in
+	--wallpaper)
+		selected="$2"
+		shift
+		;;
+	*)
+		echo "Unknown parameter passed: $1"
+		exit 1
+		;;
+	esac
+	shift
 done
 
 # If no --wallpaper flag, use the current method to select wallpaper
 if [ -z "$selected" ]; then
-    selected=$(find ~/wallpaper -type f | grep -E "\.jpg$|\.png$" | rofi -dmenu -config ~/.config/rofi/config-wallpaper.rasi -p "Wallpapers")
+	selected=$(find -L ~/wallpaper -type f | grep -E "\.jpg$|\.png$" | rofi -dmenu -config ~/.config/rofi/config-wallpaper.rasi -p "Wallpapers")
 fi
 
 if [ "$selected" ]; then
 
-    echo "Changing theme..."
-    # ----------------------------------------------------- 
-    # Update wallpaper with pywal
-    # ----------------------------------------------------- 
-    wal -q -i $selected
+	echo "Changing theme..."
+	# -----------------------------------------------------
+	# Update wallpaper with pywal
+	# -----------------------------------------------------
+	wal -q -i $selected
 
-    # ----------------------------------------------------- 
-    # Get new theme
-    # ----------------------------------------------------- 
-    source "$HOME/.cache/wal/colors.sh"
+	# -----------------------------------------------------
+	# Get new theme
+	# -----------------------------------------------------
+	source "$HOME/.cache/wal/colors.sh"
 
-    # ----------------------------------------------------- 
-    # Copy selected wallpaper into .cache folder
-    # ----------------------------------------------------- 
-    cp $wallpaper ~/.cache/current_wallpaper.jpg   
+	# -----------------------------------------------------
+	# Copy selected wallpaper into .cache folder
+	# -----------------------------------------------------
+	cp $wallpaper ~/.cache/current_wallpaper.jpg
 
-    newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+	newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
 
-    # ----------------------------------------------------- 
-    # Set the new wallpaper
-    # ----------------------------------------------------- 
-    swww img $wallpaper \
-        --transition-bezier .43,1.19,1,.4 \
-        --transition-fps=60 \
-        --transition-type="random" \
-        --transition-duration=0.7 \
-        --transition-pos "$( hyprctl cursorpos )"
+	# -----------------------------------------------------
+	# Set the new wallpaper
+	# -----------------------------------------------------
+	swww img $wallpaper \
+		--transition-bezier .43,1.19,1,.4 \
+		--transition-fps=60 \
+		--transition-type="random" \
+		--transition-duration=0.7 \
+		--transition-pos "$(hyprctl cursorpos)"
 
-    ~/.config/waybar/launch.sh
-    sleep 1
+	~/.config/waybar/launch.sh
+	sleep 1
 
-    # ----------------------------------------------------- 
-    # Send notification
-    # ----------------------------------------------------- 
-    notify-send "Colors and Wallpaper updated" "with image $newwall"
+	# -----------------------------------------------------
+	# Send notification
+	# -----------------------------------------------------
+	notify-send "Colors and Wallpaper updated" "with image $newwall"
 
-    echo "Done."
+	echo "Done."
 fi
