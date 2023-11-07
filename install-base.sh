4068a09d0fbf1ac8ccbf62cc4653508827c32bbd
DinDotDout <joandot1998@gmail.com>
2023-11-07 13:58:51 +0100

    Some messages and auto timeshift config and backup prompt

--- install-base.sh
+++ install-base.sh
@@ -36,7 +36,7 @@ get_user_input() {
 }
 
 install_paru_and_aur_pcks() {
-	echo "Installing paru!"
+	echo "Installing paru"
 	git clone https://aur.archlinux.org/paru-git.git
 	(cd paru-git && makepkg -si)
 	echo "DONE!"
@@ -55,10 +55,22 @@ install_paru_and_aur_pcks() {
 	echo "DONE!"
 }
 
+configure_timeshift() {
+	echo "Configuring timeshift"
+	sudo sed -i 's/"include_btrfs_home_for_backup": "false"/"include_btrfs_home_for_backup": "true"/g' /etc/timeshift/timeshift.json
+	sudo sed -i 's/"schedule_weekly": "false"/"schedule_weekly": "true"/g' /etc/timeshift/timeshift.json
+	sudo sed -i 's/"schedule_monthly": "false"/"schedule_monthly": "true"/g' /etc/timeshift/timeshift.json
+	sudo sed -i 's/"count_monthly": "[0-9]*"/"count_monthly": "6"/g' /etc/timeshift/timeshift.json
+	sudo sed -i 's/"count_weekly": "[0-9]*"/"count_weekly": "4"/g' /etc/timeshift/timeshift.json
+	echo "DONE!"
+}
+
 configure_flatpak_and_add_repos() {
+	echo "Adding flatpak repos"
 	flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 	flatpak install --user flathub com.github.tchx84.Flatseal
 	flatpak install --user flathub com.microsoft.Edge
+	echo "DONE!"
 }
 
 add_zram() {
@@ -76,14 +88,17 @@ add_zram() {
 }
 
 prepare_dotfiles() {
+	echo "Stowing dotfiles"
 	stow -R conf-base
 	xdg-user-dirs-update
+	echo "DONE!"
 }
 
 change_to_zsh_shell() {
 	echo "Changing to zsh shell"
 	chsh -s /usr/bin/zsh
 	zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+	echo "DONE!"
 }
 
 change_display_manager() {
@@ -97,24 +112,47 @@ change_display_manager() {
 	fi
 	echo "Enabling new display manager service"
 	systemctl enable sddm.service
+	echo "DONE!"
 }
 
 add_window_manager() {
+	echo "Adding selected windowmanager"
 	if [[ -n $window_manager ]]; then
 		# shellcheck disable=SC1090
 		source "$window_manager"
 	fi
+	echo "DONE!"
+}
+
+create_snapshot() {
+	while true; do
+		read -rp "Do you want to create a Timeshift snapshot? (yY/nN) " yn
+		case $yn in
+		[yY]*)
+			snapshot.sh
+			break
+			;;
+		[nN]*)
+			break
+			;;
+		*) echo "Please answer yY or nN." ;;
+		esac
+	done
+
 }
 
 main() {
 	get_user_input
 	install_paru_and_aur_pcks
+	configure_timeshift
 	configure_flatpak_and_add_repos
 	add_zram
 	prepare_dotfiles
 	change_to_zsh_shell
 	change_display_manager
 	add_window_manager
+	create_snapshot
+
 	echo "Finished"
 	echo "All options will be enabled after rebooting"
 }
