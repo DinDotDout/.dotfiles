4cf2d1153587cad66c9d555846c20ee8d55c3944
DinDotDout <joandot1998@gmail.com>
2023-11-08 12:59:12 +0100

    changed things in gtk theme

--- conf-hyprland/.config/hypr/gtk.sh
+++ conf-hyprland/.config/hypr/gtk.sh
@@ -3,8 +3,8 @@
 gnome_schema="org.gnome.desktop.interface"
 
 gsettings set "$gnome_schema" gtk-theme "Breeze-Dark"
-gsettings set "$gnome_schema" icon-theme "kora"
-gsettings set "$gnome_schema" cursor-theme "Catppuccin-Mocha-Flamingo-Cursors"
+gsettings set "$gnome_schema" icon-theme "Papirus-Dark"
+gsettings set "$gnome_schema" cursor-theme "Catppuccin-Mocha-Dark"
 gsettings set "$gnome_schema" font-name "Cantarell 11"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
