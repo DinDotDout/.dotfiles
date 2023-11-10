43b3fb9c635bf0201a0750c9bf45a750ad68a74d
DinDotDout <joandot1998@gmail.com>
2023-11-10 14:30:30 +0100

    Warning I3 unmaintained

--- install-i3.sh
+++ install-i3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+# WARN: My I3 configuration has been unmaintained for a while, proceed with caution
 sudo pacman --needed --noconfirm -S polybar i3-gaps picom flameshot arandr xorg-xrandr xclip
 paru --needed --noconfirm -S i3lock-color
 stow -R conf-i3
 echo "Addin permissions to lock screen script"
-chmod +x $HOME/.local/bin/lock
