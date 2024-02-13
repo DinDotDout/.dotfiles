b4ef6942b5c59ee205686df1d02c88f26a048151
DinDotDout <joandot1998@gmail.com>
2024-02-13 19:44:04 +0100

    wallpaper script organization

--- conf-hyprland/.scripts/wallpaper.sh
+++ conf-hyprland/.scripts/wallpaper.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-wal_flags="--backend colorz -q -i"
-case $1 in
-# Load wallpaper from .cache of last session
-"init")
+WAL_FLAGS="--backend colorz -qt -i"
+
+function load_wallpaper {
 	if [ -f ~/.cache/current_wallpaper.jpg ]; then
-		wal $wal_flags~/.cache/current_wallpaper.jpg
+		wal $WAL_FLAGS~/.cache/current_wallpaper.jpg
 	else
-		wal $wal_flags ~/wallpaper/
+		wal $WAL_FLAGS ~/wallpaper/
 	fi
-	;;
+}
 
-# Select wallpaper with rofi
-"select")
-
-	# selected=$(find -L ~/wallpaper -type f | grep -E "\.jpg$|\.jpeg$|\.png$" | rofi -dmenu -replace -config ~/.config/rofi/config-wallpaper.rasi -p "Wallpapers")
+function select_wallpaper {
 	selected=$(find -L "$HOME/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | while read rfile; do
 		echo -en "$rfile\x00icon\x1f$HOME/wallpaper/${rfile}\n"
 	done | rofi -dmenu -replace -config ~/.config/rofi/config-wallpaper.rasi)
@@ -23,54 +19,61 @@ case $1 in
 		echo "No wallpaper selected"
 		exit
 	fi
-	wal $wal_flags ~/wallpaper/"$selected"
-	;;
+	wal $WAL_FLAGS ~/wallpaper/"$selected"
+}
 
-# Randomly select wallpaper
+function random_wallpaper {
+	wal --recursive $WAL_FLAGS ~/wallpaper/
+}
+
+function set_new_wallpaper {
+	source "$HOME/.cache/wal/colors.sh" # Import colors and wallpaper
+	cp "$wallpaper" ~/.cache/current_wallpaper.jpg
+	transition_type="wipe"
+	swww img "$wallpaper" \
+		--transition-bezier 0.4,1.10,0.9,0.5 \
+		--transition-type=$transition_type \
+		--transition-duration=1.0 \
+		--transition-fps=60
+}
+
+# Update telegram and firefox themes
+function update_themes {
+	~/.config/waybar/launch.sh # waybar theme
+
+	# kitty theme
+	PIDS=$(pgrep kitty)
+	for PID in $PIDS; do
+		kill -SIGUSR1 $PID
+	done
+
+	pywalfox update # firefox theme
+	walogram        # telegram theme
+
+	# dunst theme
+	pkill dunst
+	dunst -conf ~/.cache/wal/colors-dunstrc &
+}
+
+function send_notification {
+	newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+	notify-send "Colors and Wallpaper updated" "with image $newwall"
+	echo "Done."
+}
+
+case $1 in
+"init")
+	load_wallpaper
+	;;
+"select")
+	select_wallpaper
+	;;
 *)
-	wal --recursive $wal_flags ~/wallpaper/
+	random_wallpaper
 	;;
-
 esac
 
 echo "Changing theme..."
-# Find the process ID (PID) of the kitty instance you want to reload
-PIDS=$(pgrep kitty)
-
-# Send the SIGUSR1 signal (reload config) to all kitty processes
-for PID in $PIDS; do
-	kill -SIGUSR1 $PID
-done
-
-# -----------------------------------------------------
-# Get new theme
-# -----------------------------------------------------
-source "$HOME/.cache/wal/colors.sh"
-# -----------------------------------------------------
-# Copy selected wallpaper into .cache folder
-# -----------------------------------------------------
-cp "$wallpaper" ~/.cache/current_wallpaper.jpg
-
-newwall=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
-
-~/.config/waybar/launch.sh
-pkill dunst
-dunst -conf ~/.cache/wal/colors-dunstrc &
-# -----------------------------------------------------
-# Set the new wallpaper
-# -----------------------------------------------------
-transition_type="wipe"
-swww img "$wallpaper" \
-	--transition-bezier 0.4,1.10,0.9,0.5 \
-	--transition-type=$transition_type \
-	--transition-duration=1.0 \
-	--transition-fps=60
-
-pywalfox update
-walogram
-
-# -----------------------------------------------------
-# Send notification
-# -----------------------------------------------------
-notify-send "Colors and Wallpaper updated" "with image $newwall"
-echo "Done."
+set_new_wallpaper
+update_themes
+send_notification
