ea3b19cd6c9843817457a71c98fe06b76e0d202e
DinDotDout <joandot1998@gmail.com>
2024-02-13 19:44:17 +0100

    redshift script notifications

--- conf-hyprland/.scripts/redshift.sh
+++ conf-hyprland/.scripts/redshift.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+id=1
 
 # Check if redshift is running
 if pgrep -x "redshift" >/dev/null; then
 	# Kill it if it is
 	pkill -x "redshift"
+	# Replace the notification with the defined ID
+	notify-send -r "$id" "Redshift   "
 else
 	# Launch it if it's not
-	redshift -O 5000k
+	redshift -O 5000k &
+	# Replace the notification with the same ID
+	notify-send -r "$id" "Redshift   "
 fi
