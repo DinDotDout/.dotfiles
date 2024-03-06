47985a63d630a3a4f8c17b3b2fed1477252a0955
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-03-06 17:42:40 +0100

    Telegram scratch broken again (Hotfix)

--- wm_scripts/toggle_scratch.sh
+++ wm_scripts/toggle_scratch.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-
 if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "No parameter provided."
 	exit 1
 fi
+
 app="$1"
 scratch_name="$2"
 
+# If not running, toggle the scratch_name
 if ! pgrep -fx "$app" >/dev/null; then
 	hyprctl dispatch exec pypr toggle "$scratch_name"
 	echo "create"
 	exit 0
 fi
 
-# If no telegram spawn
+# If the application is not running, start it
 if ! hyprctl clients | grep -q "$scratch_name"; then
-	hyprctl dispatch exec "$app"
-	echo "send"
-else
+    killall "$app"
+	# hyprctl dispatch exec "$app"
+	# echo "send"
+else # If the application is running, toggle the scratch_name
 	hyprctl dispatch exec pypr toggle "$scratch_name"
 	echo "toggle"
 fi
