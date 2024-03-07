53684a990a7ce0f49a7011f3de37ade6b93a10c6
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-03-07 13:51:31 +0100

    locksreentime lock path corrected

--- wm_scripts/lockscreentime.sh
+++ wm_scripts/lockscreentime.sh
@@ -3,7 +3,7 @@ timeswaylock=180
 timeoff=300
 
 if [ -f "/usr/bin/swayidle" ]; then
-	swayidle -w timeout $timeswaylock "$HOME/.scripts/swaylock.sh" timeout $timeoff 'hyprctl dispatch dpms off'
+	swayidle -w timeout $timeswaylock "$HOME/.dotfiles/wm_scripts/swaylock.sh" timeout $timeoff 'hyprctl dispatch dpms off'
 	# resume 'hyprctl dispatch dpms on'
 else
 	echo "Swayidle not installed."
